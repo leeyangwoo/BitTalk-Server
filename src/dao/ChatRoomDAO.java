06c3d69dfc5259cff1bc2da68c696b1236c6972e
leeyangwoo <lalunamaki@gmail.com>
2016-02-12 11:58:35 +0900

    파일명

--- src/dao/ChatRoomDAO.java
+++ src/dao/ChatRoomDAO.java
@@ -1,5 +1,26 @@
 package dao;
 
-public class ChatRoomDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import common.DBUtil;
+
+public class ChatRoomDAO {
+	public int setChatroom(){
+		int resultCount = 0;	
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		try{
+			conn = DBUtil.getConnection();
+			String sql = "INSERT INTO chatroom(numparticipant) VALUES(2);";
+			stmt = conn.prepareStatement(sql);
+			
+			resultCount = stmt.executeUpdate();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			DBUtil.close(conn, stmt);
+		}
+		return resultCount;
+	}
 }
--- src/dao/MemberDAO.java
+++ src/dao/MemberDAO.java
@@ -10,7 +10,7 @@ import common.DBUtil;
 import dto.MemberDTO;
 
 public class MemberDAO {
-	public List<MemberDTO> getMemberList(){
+	public List<MemberDTO> getMemberList(){                 //모든 회원리스트
 		List<MemberDTO> memberList = new ArrayList<>();
 		Connection conn = null;
 		PreparedStatement stmt = null;
@@ -36,7 +36,33 @@ public class MemberDAO {
 		return memberList;
 	}
 	
-	public MemberDTO getMember(String mid){
+	public List<MemberDTO> getMemberList(String mid){           // 문자열포함된 아이디로 리스트 검색
+		List<MemberDTO> memberList = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try{
+			conn = DBUtil.getConnection();
+			stmt = conn.prepareStatement("SELECT * FROM member WHERE mid LIKE '%?%';");
+			stmt.setString(1, mid);
+			rs = stmt.executeQuery();
+			while(rs.next()){
+				MemberDTO member = new MemberDTO();
+				member.setMno(rs.getInt(1));
+				member.setMid(rs.getString(2));
+				member.setMpasswd(rs.getString(3));
+				member.setMname(rs.getString(4));
+				memberList.add(member);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			DBUtil.close(conn, stmt, rs);
+		}
+		return memberList;
+	}
+	
+	public MemberDTO getMember(String mid){                    //회원 아이디로 검색
 		MemberDTO member = null;
 		Connection conn = null;
 		PreparedStatement stmt = null;
@@ -65,7 +91,7 @@ public class MemberDAO {
 		return member;
 	}
 	
-	public MemberDTO getMember(int mno){
+	public MemberDTO getMember(int mno){                     //회원번호로 검색
 		MemberDTO member = null;
 		Connection conn = null;
 		PreparedStatement stmt = null;
@@ -94,13 +120,13 @@ public class MemberDAO {
 		return member;
 	}
 	
-	public int setMember(MemberDTO member){
+	public int setMember(MemberDTO member){                    //회원 가입
 		int resultCount = 0;
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		try{
 			conn = DBUtil.getConnection();
-			String sql = "insert into member(mid, mpasswd, mname) values(?,?,?)";
+			String sql = "INSERT INTO member(mid, mpasswd, mname) VALUES(?,?,?)";
 			stmt = conn.prepareStatement(sql);
 			stmt.setString(1, member.getMid());
 			stmt.setString(2, member.getMpasswd());
